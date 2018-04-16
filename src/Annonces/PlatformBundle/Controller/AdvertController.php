0e99d443b3893d7aecccd2761320df90a9d6e331
einiass <einiass@hotmail.com>
2018-04-16 15:13:22 +0200

    Helloworld : le router de symfony

--- src/Annonces/PlatformBundle/Controller/AdvertController.php
+++ src/Annonces/PlatformBundle/Controller/AdvertController.php
@@ -9,17 +9,38 @@ use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
-    public function indexAction()
+    public function indexAction($page)
     {
         //$content = $this->get('templating')->render('OCPlatformBundle:Advert:index.html.twig');
-		//$content = $this->get('templating')->render('@AnnoncesPlatform/Advert/index.html.twig');
-		$content = $this->renderView('@AnnoncesPlatform/Advert/index.html.twig', array('nom' => 'Wizou'));
+		$content = $this->renderView('@AnnoncesPlatform/Advert/index.html.twig', array('nom' => 'Wizou','page' => $page));
 		return new Response($content);
     }
 	
-	public function logoutAction()
+	public function viewAction($id)
     {
-		$content = $this->renderView('@AnnoncesPlatform/Advert/logout.html.twig', array('nom' => 'Wizou'));
+		$content = $this->renderView('@AnnoncesPlatform/Advert/view.html.twig', array('id' => $id));
 		return new Response($content);
     }
+	
+	public function viewCourtTextAction($_local, $annee, $courtText, $_format)
+    {
+		return new Response("afficher  :  en ".$_local."   ".$annee."  ".$courtText."  ".$_format);
+    }
+	
+	
+	public function ajouterAction()
+    {
+		return new Response("ajouter annonce formulaire");
+    }
+	
+	public function modifierAction($id)
+    {
+		return new Response("modifier : ".$id);
+    }
+	
+	public function supprimerAction($id)
+    {
+		return new Response("supprimer  : ".$id);
+    }
+	
 }
